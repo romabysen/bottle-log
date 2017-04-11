1274a7e6afa3f077a8c04d84b70293d29cd86d21
Lars Hansson <romabysen@gmail.com>
2017-04-11 15:30:17 +0800

    Add long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+
+def read(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+
+
 setup(
     name='bottle-log',
     version='1.0.0',
     license='New BSD',
     py_modules=['bottle_log'],
     description='Improved logging for Bottle.',
+    long_description=read('README.rst'),
     author='Lars Hansson',
     author_email='romabysen@gmail.com',
     url='https://github.com/romabysen/bottle-log',
